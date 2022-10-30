67648d5bcfa048f3bd26d70c9c07316350cd8d06
Tamas Kiss <tamas.kiss@wirtek.com>
2022-10-30 21:20:17 +0200

    First Setup

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const methodOverride = require('method-override');
+
+const ExpressError = require('./utils/ExpressError');
 
 const app = express();
 
@@ -6,8 +12,22 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('home');
+});
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something Went Wrong!';
+    res.status(statusCode).render('error', { err });
 });
 
 app.listen(3000, () => {
