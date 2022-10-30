bb701a57360f073d00147d410fdf9068378b6c4a
Tamas Kiss <tamas.kiss@wirtek.com>
2022-10-30 20:28:57 +0200

    Setting up

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000!');
+});
