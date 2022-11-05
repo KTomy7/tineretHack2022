f6156deb9492a23fe76a3047653ba8663d078ebb
Tamas Kiss <tamas.kiss@wirtek.com>
2022-11-05 16:21:23 +0200

    Admin page

--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,20 @@
 const express = require('express');
-const passport = require('passport');
-const catchAsync = require('../utils/catchAsync');
-const Admin = require('../models/admin');
+const Project = require('../models/project');
+const { route } = require('./login');
 
 const router = express.Router();
 
-router.get('/login', (req, res) => {
-    res.render('auth/login');
+router.get('/index', async (req, res) => {
+    const projects = await Project.find({});
+    res.render('./admin/index', { projects });
 });
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), async(req, res) => {
-    res.redirect('/');
-    console.log("Successful login!!!");
+router.get('/new', (req, res) => {
+    res.render('new');
 });
 
-router.get('/logout', (req, res, next) => {
-    req.logout(function (err) {
-        if (err) {
-            return next(err);
-        }
-    });
-    console.log('You have been logged out!');
-    res.redirect('/admin');
+route.post('/', (req, res) => {
+    
 });
 
 module.exports = router; 
---
+++ routes/login.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const passport = require('passport');
+const catchAsync = require('../utils/catchAsync');
+const Admin = require('../models/admin');
+
+const router = express.Router();
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), async(req, res) => {
+    res.redirect('/admin/index');
+    console.log("Successful login!!!");
+});
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+    });
+    console.log('You have been logged out!');
+    res.redirect('/');
+});
+
+module.exports = router; 
